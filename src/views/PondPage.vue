7e9132cea142607d120c2f872b3093dbfab5ec48
jonathanlael9 <jonathanlaelaruan@gmail.com>
2022-11-16 13:44:13 +0700

    first commit

---
+++ src/views/PondPage.vue
@@ -0,0 +1,280 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <ion-title>Kolam</ion-title>
+            </ion-col>
+            <ion-col size="1">
+              <img src="public/Calendar.svg" alt="">
+            </ion-col>
+            <ion-col size="1.1">
+              <img src="public/img/Vector.svg" alt="">
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-toolbar>
+    </ion-header>    
+    <ion-content :fullscreen="true">
+      <ion-card color="dark">
+        <ion-item lines="full" color="dark">
+          <ion-label v-model="Nama_Kolam"><strong>Alpha</strong></ion-label>
+          <ion-button  size="small" color="success" v-model="Status">
+            Aktif
+          </ion-button>
+        </ion-item>
+        <ion-card-content>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="1">
+                <img src="public/img/Calendar.svg" alt="">
+              </ion-col>
+              <ion-col size="auto" v-model="Tanggal">
+                01-09-2022
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Timesheet.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="waktu">
+                90 hari
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/fish.png"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="total_activation">
+              200 Ekor
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card> 
+
+        <ion-card color="dark">
+        <ion-item lines="full" color="dark">
+          <ion-label v-model="Nama_Kolam"><strong>Beta</strong></ion-label>
+          <ion-button slot="end" size="small" color="danger" v-model="Status">
+            Tidak Aktif
+          </ion-button>
+        </ion-item>
+        <ion-card-content>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Calendar.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="Tanggal">
+               01-09-2022
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Timesheet .svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="waktu">
+                90 hari
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Whole Fish.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="total_activation">
+                200 Ekor
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card> 
+
+        <ion-card color="dark">
+        <ion-item lines="full" color="dark">
+          <ion-label v-model="Nama_Kolam"><strong>Charlie</strong></ion-label>
+          <ion-button slot="end" size="small" color="success">
+            Aktif
+          </ion-button>
+        </ion-item>
+        <ion-card-content>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Calendar.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="Tanggal">
+                01-09-2022
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Timesheet .svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="waktu">
+                90 hari
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Whole Fish.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="total_activation">
+                200 Ekor
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card> 
+
+        <ion-card color="dark">
+        <ion-item lines="full" color="dark">
+          <ion-label v-model="Nama_Kolam"><strong>Delta</strong></ion-label>
+          <ion-button slot="end" size="small" color="success" v-model="Status">
+            Aktif
+          </ion-button>
+        </ion-item>
+        <ion-card-content>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Calendar.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="Tanggal">
+              01-09-2022
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Timesheet .svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="waktu">
+              90 hari
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Whole Fish.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="total_activation">
+              200 Ekor
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card> 
+
+        <ion-card color="dark">
+        <ion-item lines="full" color="dark">
+          <ion-label v-model="Nama_Kolam"><strong>Epsilon</strong></ion-label>
+          <ion-button slot="end" size="small" color="success" v-model="Status">
+            Aktif
+          </ion-button>
+        </ion-item>
+        <ion-card-content>
+          <ion-grid>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Calendar.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="Tanggal">
+              01-09-2022
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Timesheet .svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="waktu">
+              90 hari
+              </ion-col>
+            </ion-row>
+            <ion-row>
+              <ion-col size="1">
+                <ion-icon src="public/img/Whole Fish.svg"></ion-icon>
+              </ion-col>
+              <ion-col size="auto" v-model="total_activation">
+              200 Ekor
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+
+    <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+            <ion-fab-button color="tertiary">
+            
+            </ion-fab-button>
+        </ion-fab>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonFab, IonFabButton, IonIcon} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'PageSecond',
+  components: {IonContent,IonHeader,IonPage,IonTitle,IonToolbar,IonCol,IonGrid,IonRow,IonFab,IonFabButton,IonIcon
+  },
+  data() {
+    return {
+      Nama_Kolam: [],
+      Tanggal: [],
+      Status: [],
+      waktu: [],
+      total_activation: [],
+    };
+  },
+  methods: {
+    getFormData() {
+      console.log(this.Nama_Kolam);
+      console.log(this.Tanggal);
+      console.log(this.Status);
+      console.log(this.waktu);
+      console.log(this.total_activation);
+      var pond = {'name' : this.Nama_Kolam, 'Tanggal' : this.Tanggal, 'Status': this.Status, 'waktu' : this.waktu, 'total_activation' : this.total_activation}
+          console.log(pond)
+    },
+  },
+
+
+});
+</script>
+
+<style scoped>
+ ion-title{
+  font-weight: bold;
+  font-size: 30px;
+ }
+
+  ion-card{
+  border-radius: 20px;
+  margin-top: 20px;
+  font-size: 50px;
+ }
+
+ ion-icon{
+  font-size: 30px;
+ }
+
+ ion-col{
+  font-weight: bold;
+  font-size: 20px;
+ }
+
+ ion-button{
+  border-radius: 20px;
+  font-size: 15px;
+ }
+
+ ion-item{
+  font-weight: bold;
+  font-size: 30px;
+ }
+
+</style>
---
+++ src/views/Registration.vue
@@ -0,0 +1,96 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Registrasi Kolam</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-item>
+        <ion-label position="stacked">Nama Kolam</ion-label>
+        <ion-input
+          :clear-input="true"
+          placeholder="Kolam 1"
+          v-model="nama"
+        ></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label position="stacked">Lokasi Kolom</ion-label>
+        <ion-input
+          :clear-on-edit="true"
+          placeholder="Blok A"
+          v-model="lokasi"
+        ></ion-input>
+      </ion-item>
+
+      <ion-item>
+        <ion-label position="stacked">Material Kolam</ion-label>
+        <ion-select v-model="material">
+          <ion-select-option value="Tanah">Tanah</ion-select-option>
+          <ion-select-option value="Pasir">Pasir</ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label position="stacked">Bentuk Kolam</ion-label>
+        <ion-select v-model="bentuk">
+          <ion-select-option value="Bundar">Bundar</ion-select-option>
+          <ion-select-option value="Persegi">Persegi</ion-select-option>
+        </ion-select>
+      </ion-item>
+      <center><ion-button @click.prevent="getFormData()">Registrasi</ion-button></center>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonButton,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "pondRegistrationPage",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonButton,
+  },
+  data() {
+    return {
+      nama: "",
+      lokasi: "",
+      material: "",
+      bentuk: "",
+    };
+  },
+  methods: {
+    getFormData() {
+      console.log(this.nama);
+      console.log(this.lokasi);
+      console.log(this.material);
+      console.log(this.bentuk);
+      var pond = {'name' : this.nama, 'location' : this.lokasi, 'material': this.material, 'shape' : this.bentuk}
+          console.log(pond)
+    },
+  },
+});
+</script>
